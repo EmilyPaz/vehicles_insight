e13ef03a632decdb38a1f51657a9306070b0b7a0
Emily Paz <emilypaz3012@gmail.com>
2021-01-04 18:53:23 +0100

    bonus

--- vehicles.py
+++ vehicles.py
@@ -1,3 +1,14 @@
 import pandas as pd
 df = pd.read_csv('/home/emily/Bootcamp/vehicles_insight/data/vehicles.csv')
-print(df.shape)
+print(f'Data frame shape is: {df.shape}')
+
+print(f'Data frame lenght is: {len(df.index)}')
+
+print(f'Columns are: {df.columns.to_list()}')
+
+print(f'The amount of null values per column is: {df.isnull().sum()}')
+
+print(f'The minimun values in numerical columns are: {df[['Engine Displacement', 'Cylinders', 'Fuel Barrels/Year', 'City MPG', 'Combined MPG', 'CO2 Emission Grams/Mile', 'Fuel Cost/Year']].min()}')
+
+print(f'The minimun values in numerical columns are: {df[['Engine Displacement', 'Cylinders', 'Fuel Barrels/Year', 'City MPG', 'Combined MPG', 'CO2 Emission Grams/Mile', 'Fuel Cost/Year']].max()}')
+
